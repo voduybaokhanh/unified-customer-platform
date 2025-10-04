8b8f053dc749bdf6231ffded2179472bae49a430
Phong908 <117243623+voduybaokhanh@users.noreply.github.com>
2025-10-04 11:42:39 +0700

    fix issue

--- backend/src/modules/crm/crm.module.ts
+++ backend/src/modules/crm/crm.module.ts
@@ -1,9 +1,11 @@
 // backend/src/modules/crm/crm.module.ts
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { CrmService } from './crm.service';
 import { CrmController } from './crm.controller';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CrmController],
   providers: [CrmService],
   exports: [CrmService], // Export để modules khác có thể sử dụng
