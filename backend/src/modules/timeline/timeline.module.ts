8de3e8aad8ad9b944cafc3f68bb8c1da4e9fd6dc
Phong908 <117243623+voduybaokhanh@users.noreply.github.com>
2025-10-06 09:00:42 +0700

    Update timeline.module.ts

--- backend/src/modules/timeline/timeline.module.ts
+++ backend/src/modules/timeline/timeline.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TimelineService } from './timeline.service';
 import { TimelineController } from './timeline.controller';
+import { NotificationsGateway } from './notifications.gateway';
 
 @Module({
-  providers: [TimelineService],
-  controllers: [TimelineController]
+  controllers: [TimelineController],
+  providers: [TimelineService, NotificationsGateway],
+  exports: [TimelineService, NotificationsGateway],
 })
 export class TimelineModule {}
